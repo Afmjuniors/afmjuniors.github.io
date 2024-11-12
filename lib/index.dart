dd0a48625dd4fe418d90f8e59a8e57ec429c99a9
Alexandre Machado <afmjuniors@gmail.com>
2024-11-12 17:04:11 -0300

    wip

--- lib/index.dart
+++
@@ -1,7 +0,0 @@
-// Export pages
-export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
-export '/pages/login/login_widget.dart' show LoginWidget;
-export '/pages/create_account/create_account_widget.dart'
-    show CreateAccountWidget;
-export '/pages/details02_basic_layout/details02_basic_layout_widget.dart'
-    show Details02BasicLayoutWidget;
